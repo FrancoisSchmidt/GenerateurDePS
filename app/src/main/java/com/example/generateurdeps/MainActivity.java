25792405751dd6dfe8039f6b2c1556859522109f
Franz57 <francois.schmidt@outlook.com>
2020-01-15 16:51:06 +0100

    modif

--- app/src/main/java/com/example/generateurdeps/MainActivity.java
+++ app/src/main/java/com/example/generateurdeps/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.generateurdeps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,17 +15,22 @@ public class MainActivity extends AppCompatActivity {
     EditText input_non_ps;
     EditText input_ps;
     Spinner dropdown;
+    int nblines = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        this.btn_convert.findViewById();
+        this.btn_trad.findViewById();
+        this.input_non_ps.findViewById();
+        this.input_ps.findViewById();
+        this.dropdown.findViewById();
     }
 
     public void change_diviseurs(View view){
-
+        Editable text = this.input_non_ps.getText();
     }
 
     public void convert (View view){

6e2c1eb6fd51704cb9b5cd2309e4bbba0c1f9af8
Franz57 <francois.schmidt@outlook.com>
2020-01-15 16:35:42 +0100

    Première modif

--- app/src/main/java/com/example/generateurdeps/MainActivity.java
+++ app/src/main/java/com/example/generateurdeps/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.generateurdeps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_convert;
+    Button btn_trad;
+    EditText input_non_ps;
+    EditText input_ps;
+    Spinner dropdown;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

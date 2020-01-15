c99a02a5fe72fb51d3789e36029afb4d9aa10012
Franz57 <francois.schmidt@outlook.com>
2020-01-15 17:08:10 +0100

    modif

--- app/src/main/java/com/example/generateurdeps/MainActivity.java
+++ app/src/main/java/com/example/generateurdeps/MainActivity.java
@@ -11,41 +11,41 @@ import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_convert;
-    Button btn_trad;
+    //Button btn_trad;
     EditText input_non_ps;
-    EditText input_ps;
-    Spinner dropdown;
-    int nblines = 0;
+//    EditText input_ps;
+//    //Spinner dropdown;
+//    int nblines ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.btn_convert.findViewById();
-        this.btn_trad.findViewById();
-        this.input_non_ps.findViewById();
-        this.input_ps.findViewById();
-        this.dropdown.findViewById();
+        //this.btn_trad.findViewById();
+        //this.input_non_ps.findViewById(R.id.editText2);
+        //this.input_ps.findViewById();
+        //this.dropdown.findViewById();
     }
 
     public void change_diviseurs(View view){
         Editable text = this.input_non_ps.getText();
+        int a = text.length();
     }
 
-    public void convert (View view){
-
-    }
-
-    public void put_ps (View view, String text){
-
-    }
-
-    public void trad (View view){
-
-    }
-
-    public void diviseurs (View view, int n){
-
-    }
+//    public void convert (View view){
+//
+//    }
+//
+//    public void put_ps (View view, String text){
+//
+//    }
+//
+//    public void trad (View view){
+//
+//    }
+//
+//    public void diviseurs (View view, int n){
+//
+//    }
 }
